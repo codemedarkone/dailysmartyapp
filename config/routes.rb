a181e298154dd26548094492f51eacf7eade10e7
Chris <codemedarkone@gmail.com>
2017-08-11 20:34:43 -0600

    created show page and posts page with links connecting them and did a ton of testing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   
-  resources :topics do 
-    resources :posts
+  resources :topics do
+    scope module: :topics do
+      resources :posts
+    end
   end
- 
+
   devise_for :users
+
+  get 'posts/new', to: 'topics/posts#new', as: 'new_post'
+  post 'posts', to: 'topics/posts#create', as: 'create_post'
+
+
   root to: 'static#home'
+  
 end
